9741bba96bac8a3f2a3a6566c2e0fd1f38665df4
Steve Eckles <steve.d.eckles@gmail.com>
2019-12-04 13:22:25 -0500

    Complete User Story 2
    This also sets up the framework to make US3 work

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// remove the start button; add the navigation buttons
 const gameStart = () => {
+  const navList = ['forest', 'gambler', 'vault', 'drain']
+  // create the nav buttons
   for (let i = 0; i < 4; i++) {
-    const arrow = document.createElement('div')
+    const arrow = document.createElement('p')
     arrow.classList.add('arrow')
     if (i < 2) {
       arrow.style.top = 320 + i * 110 + 'px'
@@ -9,12 +12,23 @@ const gameStart = () => {
       arrow.style.top = 100 + i * 110 + 'px'
       arrow.style.left = 800 + 'px'
     }
+    arrow.classList.add(navList[i])
+    arrow.textContent = navList[i]
+    arrow.addEventListener('click', () => console.log('hi'))
     document.body.getElementsByTagName('main')[0].appendChild(arrow)
   }
-  start.remove()
-  const placeHolder = document.createElement('div')
-  placeHolder.classList.add('start')
+  // explode the start button
+  start.removeEventListener('click', gameStart)
+  start.style.visibility = 'hidden'
+  const placeHolder = document.createElement('p')
+  placeHolder.style.fontSize = '5rem'
+  placeHolder.textContent = '💥'
+  placeHolder.style.position = 'absolute'
+  placeHolder.style.top = '310px'
   main.appendChild(placeHolder)
+  setTimeout(() => {
+    placeHolder.remove()
+  }, 325)
 }
 
 const start = document.getElementById('start')
